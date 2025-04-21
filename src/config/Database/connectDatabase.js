0eaf1e6fe2690195986ff9ab736528109c8cdd0e
datmicw <dat.micf@gmail.com>
2025-04-21 13:00:10 +0700

    v2 fix doten and path

--- src/config/Database/connectDatabase.js
+++ src/config/Database/connectDatabase.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-require('../../util/dotenv');
+require('../util/dotenv');
 async function connect() {
     try {
         await mongoose.connect(process.env.DB_URL, {
